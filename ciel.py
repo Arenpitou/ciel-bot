ba0bb1645af35aff9f65dc2bee0652608eff5d31
Arenpitou <arenpitou@protonmail.com>
2018-07-23 22:19:14 +0800

    Demo finished

--- ciel.py
+++ ciel.py
@@ -6,15 +6,27 @@ TOKEN = "NDcwOTIzMTk4NDMxNzU2Mjg4.Djda3g.K5-v_gdsG5df6B8GlVj1U8X9_4g"
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-@client.command(name="8ball")
-async def eight_ball() :
+@client.command(name="8ball", description="Answers a yes/no question", brief="Answers from the beyond (demo)", aliases=["eight_ball", "eightball", "8-ball"], pass_context=True)
+async def eight_ball(context) :
     possible_responses = [
-        "That is a no from me.",
-        "Not very likely.",
-        "It's too hard to tell."
-        "It's quite possible.",
-        "Definitely."
+        "That is a no from me",
+        "Not very likely",
+        "It's too hard to tell"
+        "It's quite possible",
+        "Definitely"
     ]
-    await client.say(random.choice(possible_responses))
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+@client.command()
+async def square(number) :
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
+@client.event
+async def on_ready() :
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------------")
 
 client.run(TOKEN)
